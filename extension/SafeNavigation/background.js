7bbc68f52da270e912c7ed595c66a548bf161b4a
mouayo15 <mouadilayoub15@gmail.com>
2024-10-15 11:49:42 +0200

    site

--- extension/SafeNavigation/background.js
+++
@@ -1,3 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('SafeNavigation extension installed.');
-});
--- extension/background.js
+++
@@ -1,5 +0,0 @@
-// background.js
-
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("Extension Comment Filter installed");
-});
